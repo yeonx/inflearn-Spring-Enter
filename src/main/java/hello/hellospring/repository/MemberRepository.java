925390ac9be6b4614b64a6135e5788789d7a8138
yeonx <71878202+yeonx@users.noreply.github.com>
2022-03-29 21:43:48 +0900

    Update MemberRepository.java

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -6,8 +6,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
-    Member save(Member member);
-    Optional<Member> findById(Long id);
-    Optional<Member> findByName(String name);
-    List<Member> findAll();
+    Member save(Member member); //회원을 저장하면 저장된 회원이 반환
+    Optional<Member> findById(Long id); //id로 회원을 찾는
+    Optional<Member> findByName(String name); //optional은 null처리를 위함
+    List<Member> findAll(); // 회원의 모든 리스트
 }
