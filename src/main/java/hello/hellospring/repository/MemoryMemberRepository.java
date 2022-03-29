6c0a4fcf671c3273abc18b0ebf06998c4a896141
yeonx <71878202+yeonx@users.noreply.github.com>
2022-03-29 21:44:03 +0900

    Update MemoryMemberRepository.java

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,37 +1,36 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
-import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository{
 
-    private static Map<Long, Member> store = new HashMap<>();
-    private static long sequence=0L;
+    private static Map<Long, Member> store = new HashMap<>(); //Map의 key는 Id value는 Member
+    private static long sequence=0L; // 0,1,2.. key값을 생성해주는
 
     @Override
     public Member save(Member member) {
-        member.setId(++sequence);
-        store.put(member.getId(),member);
+        member.setId(++sequence); //setId할 때 sequence값을 하나 올려줄 것임
+        store.put(member.getId(),member); //store에 저장을 할 것
         return member;
     }
 
     @Override
     public Optional<Member> findById(Long id) {
-        return Optional.ofNullable(store.get(id));
+        return Optional.ofNullable(store.get(id)); //store에 꺼내서 결과를 get. null이 반환 될 수 있으니 Optional으로 감싼다
     }
 
     @Override
     public Optional<Member> findByName(String name) {
         return store.values().stream()
-                .filter(member -> member.getName().equals(name))
-                .findAny();
+                .filter(member -> member.getName().equals(name)) //파라미터로 넘어온 name과 같은지 확인
+                .findAny(); //루프를 돌면서 찾으면 바로 반환
     }
 
     @Override
     public List<Member> findAll() {
-        return new ArrayList<>(store.values());
+        return new ArrayList<>(store.values()); //store의 맴버들을 반환
     }
 
     public void clearStore(){
